9097cb16b0a6490a19b1aee015ff80348d1fed0b
Christopher Partin <114110894+Korachof@users.noreply.github.com>
2024-05-07 11:39:00 -0700

    Create users.py
    Created the User class with some basic get methods. Will add more methods for adding different media and keeping track of some things, but need to make the object classes first to access them.

---
+++ users.py
@@ -0,0 +1,32 @@
+class User:
+  def __init__(self, name, book_list, movie_list, game_list, tv_list, event_list, collectibles_list):
+    self._name = name
+    self._book_list = book_list
+    self._movie_list = movie_list
+    self._game_list = game_list
+    self._tv_list = tv_list
+    self._event_list = event_list
+    self._collectibles_list = collectibles_list
+
+  def get_name(self):
+    return self._name
+
+  def get_book_list(self):
+    return self._book_list
+
+  def get_movie_list(self):
+    return self._movie_list
+
+  def get_game_list(self):
+    return self._game_list
+
+  def get_tv_list(self):
+    return self._tv_list
+
+  def get_event_list(self):
+    return self._event_list
+
+  def get_collectibles_list(self):
+    return self._collectibles_list
+
+  
