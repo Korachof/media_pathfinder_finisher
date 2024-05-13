a69f9bf1525135f13df61b205371a5c17c9ade30
Christopher Partin <christopherpartin11@gmail.com>
2024-05-13 11:20:54 -0700

    Changed the Cards attributes to match those in the JSON file. Added an add_card function in CardSet that is currently commented out until get_card_from_db is finished. This function will read from the card_set.json file and grab cards from it to fill an entire set.

--- cards.py
+++ cards.py
@@ -1,33 +1,41 @@
 class CardSet:
-  def __init__(self, name, set_list):
+  def __init__(self, name, set_list_hash):
     self._name = name
-    self._set_list = set_list
+    self._set_list_hash = set_list_hash
 
   def get_name(self):
     return self._name
 
   def get_set_list(self):
-    return self._set_list
+    return self._set_list_hash
+
+  def get_card_from_db(self):
+
+  
+  # def add_card(self, title, category, trait, rarity, card_num):
+  #  self._set_list[title] = Cards(title, category, trait, rarity, card_num)
+
+
     
 class Cards:
-  def __init__(self, name, attack, ac, rarity, description):
-    self._name = name
-    self._attack = attack
-    self._ac = ac
+  def __init__(self, title, category, trait, rarity, card_num):
+    self._title = title
+    self._category = category
+    self._trait = trait
     self._rarity = rarity
-    self._description = description
+    self._card_num = card_num
 
   def get_name(self):
     return self._name
 
-  def get_attack(self):
-    return self._attack
+  def get_category(self):
+    return self._category
 
-  def get_ac(self):
-    return self._ac
+  def get_trait(self):
+    return self._trait
 
   def get_rarity(self):
     return self._rarity
 
-  def get_description(self):
-    return self._description
+  def get_card_num(self):
+    return self._card_num
