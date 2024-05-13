bd57b458a08d5131fa51efa19caba090e265fdaa
Christopher Partin <christopherpartin11@gmail.com>
2024-05-13 12:00:46 -0700

    Implemented the rest of add_card_from_db to create the card object and add it to the set list.
    Will test this thoroughly later, to make sure it works and debug.

--- cards.py
+++ cards.py
@@ -19,18 +19,19 @@ class CardSet:
     
     return card_list_data
     
-  def add_card_from_db(self, card_list_data):
+  def add_cards_from_db(self, card_list_data):
+    for card in card_list_data[self._name]:
+      self.add_card(
+        card["title"],
+        card["category"],
+        card["trait"],
+        card["rarity"],
+        card["cardNumber"])
+
+  def add_card(self, title, category, trait, rarity, card_num):
+    self._set_list[title] = Cards(title, category, trait, rarity, card_num)
 
 
-
-
-
-  
-  # def add_card(self, title, category, trait, rarity, card_num):
-  #  self._set_list[title] = Cards(title, category, trait, rarity, card_num)
-
-
-    
 class Cards:
   def __init__(self, title, category, trait, rarity, card_num):
     self._title = title
