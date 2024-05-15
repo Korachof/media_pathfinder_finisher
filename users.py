289e349ed1a10bf7d4b9d474f90256e773d70e3d
Christopher Partin <christopherpartin11@gmail.com>
2024-05-14 17:10:55 -0700

    Added methods for add_movie and add_video_game for user. The dictionary keys will be in the form of: title: director/publisher: year
    There is no need to do this for books, as the year sholdn't make a difference, but I may change it just for consistency's sake.
    
    Additionally I added a booster_pack_list dict for User and a filter packs by set method, a get_booster_pack method.

--- users.py
+++ users.py
@@ -1,13 +1,14 @@
 import goal_objects
 
 class User:
-  def __init__(self, name: str, book_list: dict, movie_list: dict, game_list: dict, tv_list: dict, event_list: dict, collectibles_list: dict):
+  def __init__(self, name: str, book_list: dict, movie_list: dict, game_list: dict, tv_list: dict, event_list: dict, booster_pack_list: dict, collectibles_list: dict):
     self._name = name
     self._book_list = book_list
     self._movie_list = movie_list
     self._game_list = game_list
     self._tv_list = tv_list
     self._event_list = event_list
+    self._booster_pack_list = booster_pack_list
     self._collectibles_list = collectibles_list
 
   def get_name(self):
@@ -27,9 +28,19 @@ class User:
 
   def get_event_list(self):
     return self._event_list
+  
+  def get_booster_pack_list(self):
+    return self._booster_pack_list
 
   def get_collectibles_list(self):
     return self._collectibles_list
+  
+  def filter_booster_packs_by_set(self, set_name: str):
+    set_list = []
+    for pack in self._booster_pack_list[set_name]:
+      set_list.append(pack)
+
+      return set_list
 
   def filter_collectibles_by_set(self, set_name: str):
     set_list = []
@@ -80,6 +91,29 @@ class User:
       self._book_list[f"{name}: {author}"].incr_times_finished()
       self._book_list[f"{name}: {author}"].update_rating(rating)
       return f"The Book {name} by {author} has been updated"
+    
+  def add_movie(self, title: str, director: str, year: int, completion_hours: int, rating: int):
+    if f"{title}: {director}: {year}" not in self._movie_list:
+      self._movie_list[f"{title}: {director}: {year}"] = goal_objects.Movies(title, director, year, completion_hours, completion_hours, 1, rating)
+      return f"The Movie {title} by {director} from {year} has been added"
+    
+    else:
+      self._movie_list[f"{title}: {director}: {year}"].add_total_hours(completion_hours)
+      self._movie_list[f"{title}: {director}: {year}"].incr_times_finished()
+      self._movie_list[f"{title}: {director}: {year}"].update_rating(rating)
+      return f"The Movie {title} by {director} from {year} has been updated"
+    
+  def add_video_game(self, title: str, publisher: str, year: int, game_system: str, completion_hours: int, rating: int):
+    if f"{title}: {publisher}: {year}" not in self._game_list:
+      self._game_list[f"{title}: {publisher}: {year}"] = goal_objects.VideoGames(title, publisher, year, game_system, completion_hours, completion_hours, 1, rating)
+      return f"The Video Game {title} by {publisher} from {year} has been added"
+    
+    else:
+      self._game_list[f"{title}: {publisher}: {year}"].add_total_hours(completion_hours)
+      self._game_list[f"{title}: {publisher}: {year}"].incr_times_finished()
+      self._game_list[f"{title}: {publisher}: {year}"].update_rating(rating)
+      return f"The Video Game {title} by {publisher} from {year} has been updated"
+
 
   
 Korachof = User("Korachof", {}, {}, {}, {}, {}, {})
