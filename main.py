a70ea55cda0ba7f05e357a04b7c7b1c9bbb1521b
Christopher Partin <christopherpartin11@gmail.com>
2024-07-26 08:46:38 -0700

    Worked some on the script! It's good to be back to this program after I took a bit of a break to work on pseudo code for my cat caferet game.
    For now, I'm setting the implementation of a UIx and focusing on just a terminal UI for the prototype. For now just slowly crafting a skeleton for the script. Everything thus far appears to be working correctly, so just need to keep pushing on!

---
+++ main.py
@@ -0,0 +1,89 @@
+import users
+import sys
+
+def main():
+    print("Hello, please select an option below by typing the corresponding number and pressing ENTER\n")
+    print("1) Create New User")
+    print("2) Load User")
+    print("3) Exit")
+    user_input = input()
+
+    if user_input == "1":
+        name_check = None
+
+        while name_check != "1":
+            print("What is your name? (Type your name and press ENTER)")
+            user_name = input()
+            print(f"Is {user_name} correct? Type 1 for YES and 2 for NO")
+            name_check = input()
+
+            if name_check == "1":
+                user_profile = users.User(user_name, {}, {}, {}, {}, {}, {}, {})
+                profile_menu(user_profile)
+
+    elif user_input == "2":
+        pass
+
+    elif user_input == "3":
+        exit_program()
+        
+
+def profile_menu(user_profile):
+    print("Please select an option below by typing the corresponding number and pressing ENTER\n")
+    user_input = None
+    print("1) Add Book")
+    print("2) Add Movie")
+    print("3) Add TV Show Season")
+    print("4) Add Video Game")
+    print("5) Add Event")
+    print("6) View Finished Goals")
+    print("7) View Card Collection")
+    print("8) Edit User")
+    print("9) Exit")
+
+    while user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5" and user_input != "6" and user_input != "7" and user_input != "8" and user_input != "9":
+        user_input = input()
+
+    if user_input == "1":
+        add_book_form(user_profile)
+    elif user_input == "2":
+        pass
+    
+def add_book_form(user_profile):
+    print("Please select an option below by typing the corresponding number and pressing ENTER\n")
+    print("1) Add Book Details")
+    print("2) Go Back to Previous Menu")
+
+    user_input = None
+
+    while user_input != "1" and user_input != "2":
+        user_input = input()
+
+    if user_input == "1":
+        print("Please add the relevant information below next to each field and press ENTER\n")
+        title = input("Book Title: ")
+        author = input("Author: ")
+        page_count = input("Page Count: ")
+        year = input("Year Published: ")
+        date_finished = input("Date Finished (month/day/4-digit year): ")
+        hours = input("Approximate Hours Read Before Completion (numbers only): ")
+        rating = input("Rating (1-5, 5 being best): ")
+
+        user_profile.add_book(title, author, page_count, year, date_finished, hours, rating)
+
+    elif user_input == "2":
+        profile_menu()
+
+    
+def exit_program():
+    print("Are you sure you want to exit? Type 1 for YES and 2 for NO")
+    exit_check = input()
+
+    if exit_check == "1":
+        sys.exit()
+    else:
+        main()
+
+
+if __name__ == "__main__":
+    main()
